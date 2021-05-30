11523f4cbae54ac2cfa0cedb08c0fc8a418cecc4
kiranwagle <39842853+kiranwagle@users.noreply.github.com>
2021-05-30 13:51:46 -0600

    Update Hello.java
    updated to test auto trigger build

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@
     public static void main(String[] args) {
 
       System.out.println("This is a java app created using Docker");
-
+      System.out.println("This is a an update to test to auto trigger build");
     }
 
   }
